44c9a9d259ddb628c3c0d1d7b6aa84a84b2db1eb
Rajneesh Rajan <theloneprince123@gmail.com>
2026-02-26 13:52:38 +0530

    SetUp Background State

---
+++ src/background/background.js
@@ -0,0 +1,22 @@
+// Default state
+const DEFAULT_STATE = {
+  isEnabled: true,
+};
+
+browser.runtime.onInstalled.addListener(() => {
+  browser.storage.local.get("isEnabled").then((result) => {
+    if (result.isEnabled === undefined) {
+      browser.storage.local.set(DEFAULT_STATE);
+    }
+  });
+});
+
+browser.runtime.onMessage.addListener((message, sender) => {
+  if (message.type === "GET_STATE") {
+    return browser.storage.local.get("isEnabled");
+  }
+
+  if (message.type === "SET_STATE") {
+    return browser.storage.local.set({ isEnabled: message.value });
+  }
+});
